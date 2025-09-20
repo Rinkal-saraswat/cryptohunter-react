8c02a1c369dc2d950f9ae7d7a750d7d93e7c70f8
Rinkal Saraswat <121511938+Rinkal-saraswat@users.noreply.github.com>
2025-09-21 02:20:54 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,8 @@
+const API_KEY = process.env.REACT_APP_CG_API_KEY;
+
+
 export const CoinList = (Currency) =>
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${Currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false?x_cg_demo_api_key=CG_USdak4nxoK8F3qGJzKYHHcwW`;
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${Currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false?x_cg_demo_api_key=${API_KEY}`;
 
 export const SingleCoin = (id) =>
   `https://api.coingecko.com/api/v3/coins/${id}`;
@@ -9,5 +12,5 @@ export const HistoricalChart = (id, days = 365, Currency) =>
 
 export const TrendingCoins = (Currency) =>
  //  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${Currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
-//`http://api.coingecko.com/api/v3/coins/markets?vs_currency=${Currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key=CG_USdak4nxoK8F3qGJzKYHHcwW`
-  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${Currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false?x_cg_demo_api_key=CG_USdak4nxoK8F3qGJzKYHHcwW`;  
+//`http://api.coingecko.com/api/v3/coins/markets?vs_currency=${Currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false&x_cg_demo_api_key=${API_KEY}`
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${Currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false?x_cg_demo_api_key=${API_KEY}`;  
